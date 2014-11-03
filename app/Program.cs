dbf594e19497ba05f0f2e46179d8a96f27a52a12
925coder <925coder@gmail.com>
2014-11-03 01:01:06 +0000

    exploring wpf Application

--- app/Program.cs
+++ app/Program.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace app
 {
   class Program
   {
+    [STAThread()]
     static void Main(string[] args)
     {
+      Application app = new Application();
+      Window win = new Window();
+      win.Title = "aloha wpf";
+      app.Run(win);
     }
   }
 }
